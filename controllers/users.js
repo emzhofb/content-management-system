298198d46ddd339c97080468f3e06f2a0dbc4866
ikhda <ikhdamuhammad@gmail.com>
2019-07-26 20:48:42 +0700

    testing register check and destroy routes

--- controllers/users.js
+++ controllers/users.js
@@ -2,14 +2,18 @@ const jwt = require('jsonwebtoken');
 
 exports.postRegister = (req, res, next) => {
   const { email, password, retypepassword } = req.body;
-  
+
   let token;
 
   if (email && password == retypepassword) {
-    token = jwt.sign({
-      data: { email }
-    }, 'secret', { expiresIn: '1h' });
-    
+    token = jwt.sign(
+      {
+        data: { email }
+      },
+      'secret',
+      { expiresIn: '1h' }
+    );
+
     res.status(200).json({
       data: {
         email: email
@@ -17,7 +21,7 @@ exports.postRegister = (req, res, next) => {
       token: token
     });
   } else {
-    res.status(400).json({ message: 'Invalid register.' })
+    res.status(400).json({ message: 'Invalid register.' });
   }
 };
 
@@ -28,13 +32,16 @@ exports.postLogin = (req, res, next) => {
 exports.postCheck = (req, res, next) => {
   const { token } = req.body;
 
-  jwt.verify(token, 'secret', (err, decoded)=> {
-    if (err) res.status(401).json({ message: 'Invalid token.' });
-    
+  jwt.verify(token, 'secret', (err, decoded) => {
+    if (err) return res.status(401).json({ message: 'Invalid token.' });
+
     res.status(200).json({ valid: 'true' });
-  })
+  });
 };
 
 exports.getDestroy = (req, res, next) => {
-  const { token } = req.body;
-}
+  let { token } = req.body;
+  token = null;
+
+  res.status(200).json({ logout: true });
+};
--- test/user.js
+++ test/user.js
@@ -6,12 +6,18 @@ const expect = chai.expect;
 
 chai.use(chaiHttp);
 
-describe('Users', () => {
+let token;
+
+describe('Register', () => {
   it('Should be logged in and got the token', () => {
     chai
       .request(app)
       .post('/api/users/register')
-      .send({ email: 'ikhdamuhammad@gmail.com', password: '1234', retypepassword: '1234' })
+      .send({
+        email: 'ikhdamuhammad@gmail.com',
+        password: '1234',
+        retypepassword: '1234'
+      })
       .end((err, res) => {
         expect(res).to.have.status(200);
         expect(res).to.be.json;
@@ -19,21 +25,71 @@ describe('Users', () => {
         expect(res.body.data.email).to.be.a('string');
         expect(res.body).to.have.property('token');
         expect(res.body.token).to.be.a('string');
+        token = res.body.token;
       });
   });
-});
 
-describe('Users', () => {
-  it('Shouldn\'t logged in and got the token', () => {
+  it("Shouldn't logged in and token is null", () => {
     chai
       .request(app)
       .post('/api/users/register')
-      .send({ email: 'ikhdamuhammad@gmail.com', password: '1234', retypepassword: '12345' })
+      .send({
+        email: 'ikhdamuhammad@gmail.com',
+        password: '1234',
+        retypepassword: '12345'
+      })
       .end((err, res) => {
         expect(res).to.have.status(400);
         expect(res).to.be.json;
         expect(res.body).to.have.property('message');
         expect(res.body.message).to.be.a('string');
+        expect(res.body.message).to.equal('Invalid register.');
+      });
+  });
+});
+
+describe('Check', () => {
+  it('Should accepted the token', () => {
+    chai
+      .request(app)
+      .post('/api/users/check')
+      .send({ token: token })
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.have.property('valid');
+        expect(res.body.valid).to.be.a('string');
+        expect(res.body.valid).to.equal('true');
+      });
+  });
+
+  it('Should declined the token', () => {
+    chai
+      .request(app)
+      .post('/api/users/check')
+      .send({ token: null })
+      .end((err, res) => {
+        expect(res).to.have.status(401);
+        expect(res).to.be.json;
+        expect(res.body).to.have.property('message');
+        expect(res.body.message).to.be.a('string');
+        expect(res.body.message).to.equal('Invalid token.');
+      });
+  });
+});
+
+describe('Destroy', () => {
+  it('Should delete the token', () => {
+    chai
+      .request(app)
+      .get('/api/users/destroy')
+      .set('token', null)
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.have.property('logout');
+        expect(res.body.logout).to.be.a('boolean');
+        expect(res.body.logout).to.equal(true);
       });
   });
 });
